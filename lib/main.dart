e8d1c4b21d0b2b0fe9252d6d976ec83c00aab29d
kapil jhajhria <jhajhria44@gmail.com>
2019-10-03 15:45:35 +0530

    completed
    have some compilation error.

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,91 @@
 import 'package:day_4_movies_assignment/movies.dart';
 import 'package:flutter/material.dart';
+//import 'dart:io';
 import 'dart:convert';
 
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.black ,
-      appBar: AppBar(
-        title: Text(
-          'Movies Poster',
-          style: TextStyle(color: Colors.black),
-        ),
-        backgroundColor: Colors.yellow,
-      ),
-      body: Container(),// Use stateful widget you created here
-    ),
+    home: MyMovies(),
   ));
 }
 
+class MyMovies extends StatefulWidget {
+  @override
+  _MyMoviesState createState() => _MyMoviesState();
+}
+
+class _MyMoviesState extends State<MyMovies> {
+  String moviesList = MoviesList.moviesJsonList;
+  String link = "";
+  int moviesNumber = 0;
+  @override
+  Widget build(BuildContext context) {
+    List movies = jsonDecode(moviesList);
+    return SafeArea(
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text(
+            'Movies Poster',
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Colors.yellow,
+        ),
+        body: Container(
+          color: Colors.yellow.shade200,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Expanded(
+                    flex: 10,
+                    child: Image(
+//                height: 300,
+//                width: 300,
+                      image: NetworkImage('$link'),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: Card(
+                      color: Colors.yellow,
+                      child: FlatButton(
+                        child: Text('Next Movie'),
+                        onPressed: () {
+                          setState(() {
+                            link = movies[moviesNumber]['poster'];
+                            moviesNumber = moviesNumber + 1;
+                            if (moviesNumber == movies.length) moviesNumber = 0;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+//        floatingActionButton: FloatingActionButton(
+//          child: Icon(Icons.movie),
+//          onPressed: () {
+//            setState(() {
+//              link = movies[moviesNumber]['poster'];
+//              moviesNumber = moviesNumber + 1;
+//              if (moviesNumber == movies.length) moviesNumber = 0;
+//            });
+//          },
+//        ), // Use stateful widget you created here
+      ),
+    );
+  }
+}
+
 // You are given json string of movies list (see file movies.dart)
-String moviesList = MoviesList.moviesJsonList;
 
 // Hint:
 // Convert the string to List of maps using jsonDecode and then use it
 
 // Create a stateful widget called MoviesPage here
-
